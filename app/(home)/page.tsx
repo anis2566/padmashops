fde96f8ba641d7c11ce55824e54319b844e89eec
anis2566 <anichuranis1000@gmail.com>
2024-06-17 18:45:19 +0600

    faa

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,6 +9,7 @@ import { TopCategory } from "@/components/home/top-category"
 const Home = () => {
   return (
     <div className="w-full max-w-screen-xl mx-auto px-4 space-y-[30px] pb-10">
+      hello
       <Hero />
       <TopCategory />
       <PopularProducts />
